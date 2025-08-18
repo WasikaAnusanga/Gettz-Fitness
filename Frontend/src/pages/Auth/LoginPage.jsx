cd91ae381cd303318399622b3e77796fb66db168
SaweenAbey <saween910@gmail.com>
2025-08-18 06:29:10 +0530

    make front end logging page

--- Frontend/src/pages/Auth/LoginPage.jsx
+++ Frontend/src/pages/Auth/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { Toaster } from 'react-hot-toast'
+import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function LoginPage(){
@@ -27,11 +27,12 @@ export default function LoginPage(){
         console.log("Loging Success ", response.data);
         toast.success("Login Success");
         localStorage.setItem("token",response.data.token)
-            
-        const user = response.data.user;
-        if(user.role === "admin"){
+        localStorage.setItem("user", JSON.stringify(response.data.user));
 
-            navigate("/admin")
+        const user = response.data.user;
+        if(user.role === "Mmeber"){
+
+            navigate("/member/dashboard")
 
         }else{
             navigate("/")
@@ -48,14 +49,22 @@ export default function LoginPage(){
         console.log("Loging button click")
     }
     return(
-        <div className="w-full h-screen bg-amber-300 bg-cover bg-center flex">
-            <div className=" w-[50%] h-full flex items-center justify-center">
-                <h1 className="text-green-500 text-2xl animate-bounce">Welcome..........</h1>
-            </div>
-            <div className=" w-[50%] h-full flex justify-center items-center">
-                <div className="w-[450px] h-[600px] backdrop-blur-xl shadow-xl rounded-xl flex flex-col justify-center items-center">
-                    <input type="email" placeholder="Email" className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]" onChange={(e)=>{setEmail(e.target.value)}}/>
-                    <input type="password" placeholder="password" className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]" onChange={(e)=>{setPassword(e.target.value)}}/>
+    <div className="flex flex-col md:flex-row w-full min-h-screen">
+      <div className="hidden md:flex md:w-1/2 bg-red-600 relative overflow-hidden rounded-lg">
+        <div className="absolute inset-0 bg-black opacity-20"></div>
+        <div className="absolute inset-0 bg-gradient-to-r from-red-900/80 to-transparent"></div>
+        <img
+          src="https://images.unsplash.com/photo-1554344728-7560c38c1720?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Gym Equipment"
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className=" w-[50%] h-full flex justify-center items-center">
+                <div className="w-[600px] h-[700px]  shadow-xl rounded-xl flex flex-col justify-center items-center">
+                    Email :- 
+                    <input type="email" placeholder="Email" className="w-[400px] h-[50px] border border-black rounded-xl text-center m-[5px]" onChange={(e)=>{setEmail(e.target.value)}}/>
+                    <h4 className="items-left">Password:- </h4>
+                    <input type="password" placeholder="password" className="w-[400px] h-[50px] border border-black rounded-xl text-center m-[5px]" onChange={(e)=>{setPassword(e.target.value)}}/>
                     <button className="w-[400px] h-[50px] bg-green-400 text-white rounded-xl cursor-pointer"onClick={handleLoging}>Log In
                         {loading?"Loading...":"Loging"}
                     </button>
@@ -71,5 +80,5 @@ export default function LoginPage(){
             </div>
 
         </div>
-    )
+  )
 }
