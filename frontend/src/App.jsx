d465a355eb6b65386bd7da1f81ea38bb3ed5a81e
SaweenAbey <saween910@gmail.com>
2025-08-31 00:28:08 +0530

    Today Updated Video Upload Backend Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import LoginPage from './pages/loginPage'
-import AdminPage from './pages/adminPage'
+import AdminLayout from "./pages/adminPage";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Testing from './pages/testing'
 import { Toaster } from 'react-hot-toast'
 import SignupPage from './pages/Signup'
 import Homepage from './pages/homepage'
 import AdminLoginForm from './pages/admin/adminLogging'
+import Video from './pages/admin/VideoPage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,12 +18,13 @@ function App() {
     <Toaster position='top-right' />
       <Routes path="/*">
 
-        <Route path="/admin/*" element={<AdminPage />} />
+         <Route path="/admin/*" element={<AdminLayout />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/*" element={<Homepage />} />
         <Route path="/testing" element={<Testing />} />
         <Route path="/register" element={<SignupPage/>} />
         <Route path="/adminLog" element={<AdminLoginForm/>}/>
+        <Route path="/video" element={<Video/>}/>
       </Routes>
 
     </BrowserRouter>
